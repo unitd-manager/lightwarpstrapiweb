6c9eaf75f61e876a49bdaa9eae2294c1f65868e9
Bushra <bushra@unitdtechnologies.com>
2026-06-03 18:20:44 +0530

    modified

--- src/routes/about/panel-3-story.tsx
+++ src/routes/about/panel-3-story.tsx
@@ -4,7 +4,7 @@ const crabStill = "https://lightwarp3d.com/wp-content/uploads/2026/01/CrabStill.
 
 export function AboutPanelStory() {
   return (
-    <section className="mx-auto `max-w-1320px` px-15 pt-15 pb-15">
+    <section className="mx-auto max-w-[1300px] px-12 pt-10 pb-17">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
@@ -39,7 +39,8 @@ export function AboutPanelStory() {
           <h4 className="font-['Sora'] text-[20px] sm:text-[33px] lg:text-[45px] pb-[30px] text-white font-semibold leading-tight `lg:leading-60px` tracking-[-1px] `mb-30px`">
             Our Story
           </h4>
-          <p className="text-[16px] md:text-[17px] lg:text-[17px] text-white font-light `leading-24px` text-justify">
+
+          <p className="text-[14px] md:text-[15px] lg:text-[15px] font-bold text-white font-light `leading-24px` text-justify">
             Started by an alum of Texas A&amp;M University, Lightwarp 3D Studio was built on a
             vision of using real-time rendering with tools like Unreal Engine and Blender to
             create high-quality 3D stories and visuals at blazing fast speed. With our
